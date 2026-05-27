bf75d9f5afc1d6a095528e8d34ba8336734b4bea
AKJ0407 <147258775+AKJ0407@users.noreply.github.com>
2026-05-27 14:13:19 +0800

    Completed Exercises 2, 3 and 4.

--- foundations/02_addNumbers/addNumbers.js
+++ foundations/02_addNumbers/addNumbers.js
@@ -4,7 +4,7 @@ function addNumbers() {
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
+  result = a + b; // <------ EDIT THIS LINE (removed quotation marks)
 
   return result;
 }
--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,5 +1,5 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {     //EDIT THIS LINE: greater or equal to 10
     return true;
   } else {
     return false;
